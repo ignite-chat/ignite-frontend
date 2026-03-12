05bd921e2f5aa94379560a40d7e50d5116e301e6
sellauth <sellauthmail@gmail.com>
2026-03-12 22:29:37 +0000

    chore: add gap between channelemssages and channelinput

--- src/ignite/components/channel/Channel.jsx
+++ src/ignite/components/channel/Channel.jsx
@@ -71,7 +71,7 @@ const Channel = ({ channel, messageId }) => {
       <ChannelBar channel={channel} onJumpToMessage={() => {}} />
       <hr className="m-0 w-full border border-t-0 border-white/5 bg-gray-900 p-0" />
       <div className="flex min-h-0 flex-1">
-        <div className="relative flex size-full flex-1 flex-col overflow-hidden">
+        <div className="relative flex size-full flex-1 flex-col gap-4 overflow-hidden">
           <ChannelMessages channel={channel} messageId={messageId} />
           <ChannelInput channel={channel} />
         </div>
